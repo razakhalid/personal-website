ddc99564d34016d74a71bfe649815405941a0dde
razakhalid <ra97za@gmail.com>
2023-12-24 19:38:42 -0600

    updated past work section to take up full width, enabled auto carousel

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ carousels.forEach(carousel => {
         .querySelector("img")
         .offsetHeight;
 
-    // setInterval(() => {
-    //     changeSlide({
-    //         slides: carousel.querySelector("[data-carousel-slides]"),
-    //         offset: 1
-    //     })
-    // }, 7000);
+    setInterval(() => {
+        changeSlide({
+            slides: carousel.querySelector("[data-carousel-slides]"),
+            offset: 1
+        })
+    }, 7000);
 
     buttons.forEach(btn => {
         btn.addEventListener("click", () => {
