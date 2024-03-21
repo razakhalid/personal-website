f502b6ec4c24bd3e8d1a365cac6c16be81045d96
razakhalid <ra97za@gmail.com>
2024-03-21 15:43:09 -0500

    setup vue pdf plugin

--- client/src/components/Resume.vue
+++ client/src/components/Resume.vue
@@ -1,33 +1,27 @@
 <template>
-  <div>
-    <div v-if="!pdfViewerEnabled" class="my-8">
-      <p>Preview is not currently supported on mobile, to preview without downloading please visit on a desktop device.</p>
-      <p>(I'm working on it!)</p>
+  <div class="mt-8">
+    <div class="flex justify-end">
+      <a
+          class="text-lg border px-4 py-2 mb-2 rounded-lg text-sm font-space-mono uppercase inline-block
+                    hover:bg-white hover:text-gray-800 hover:cursor-pointer mt-4"
+          href="../../public/pdf/Resume%20-%20Raza%20Khalid.pdf"
+          download
+      >Download &darr;</a>
     </div>
-
-    <embed
-        src="../../public/pdf/Resume%20-%20Raza%20Khalid.pdf"
-        class="mt-16 min-h-[525px] h-full w-full sm:h-[725px] lg:h-[1125px] lg:w-[835px]"
-        v-if="pdfViewerEnabled"
-    >
-    <a
-        class="text-lg border px-4 py-2 mb-2 rounded-lg text-sm font-space-mono uppercase inline-block
-                    hover:bg-white hover:text-gray-800 hover:cursor-pointer"
-        href="../../public/pdf/Resume%20-%20Raza%20Khalid.pdf"
-        download
-    >Download &darr;</a>
+    <VuePdfEmbed :source="src"></VuePdfEmbed>
   </div>
 </template>
 <script>
+import VuePdfEmbed from 'vue-pdf-embed';
 export default {
   name: 'Resume',
   data() {
     return {
-      pdfViewerEnabled: true
+      src: '../../public/pdf/Resume%20-%20Raza%20Khalid.pdf'
     }
   },
-  created() {
-    this.pdfViewerEnabled = window.navigator.pdfViewerEnabled;
+  components: {
+    VuePdfEmbed
   }
 }
 </script>
