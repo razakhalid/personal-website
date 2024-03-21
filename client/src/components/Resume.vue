1605a6c2f62c6fe1f1e5ed7aac7fe7978f210dcc
razakhalid <ra97za@gmail.com>
2024-03-21 15:26:09 -0500

    added no preview copy

--- client/src/components/Resume.vue
+++ client/src/components/Resume.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="mt-16">
+  <div>
+    <div v-if="!pdfViewerEnabled" class="my-8">
+      <p>Preview is not currently supported on mobile, to preview without downloading please visit on a desktop device.</p>
+      <p>(I'm working on it!)</p>
+    </div>
+
     <embed
         src="../../public/pdf/Resume%20-%20Raza%20Khalid.pdf"
-        class="min-h-[525px] h-full w-full sm:h-[725px] lg:h-[1125px] lg:w-[835px]"
+        class="mt-16 min-h-[525px] h-full w-full sm:h-[725px] lg:h-[1125px] lg:w-[835px]"
+        v-if="pdfViewerEnabled"
     >
+    <a
+        class="text-lg border px-4 py-2 mb-2 rounded-lg text-sm font-space-mono uppercase inline-block
+                    hover:bg-white hover:text-gray-800 hover:cursor-pointer"
+        href="../../public/pdf/Resume%20-%20Raza%20Khalid.pdf"
+        download
+    >Download &darr;</a>
   </div>
 </template>
 <script>
@@ -11,8 +23,11 @@ export default {
   name: 'Resume',
   data() {
     return {
-      url: 'https://docs.google.com/document/d/e/2PACX-1vQ_HM9n6yZePAfVvrl2nSyPyOsFJmFI_OVeSOthG736l-7fQ-pHRN7nsqy5rba2Ij9Pu_E0vEn0hSSR/pub?embedded=true'
+      pdfViewerEnabled: true
     }
+  },
+  created() {
+    this.pdfViewerEnabled = window.navigator.pdfViewerEnabled;
   }
 }
 </script>
